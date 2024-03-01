b6a9f5b8591211f104e2ff4b757727a3d65bde75
AlexZuluaga97 <alexzulu012@gmail.com>
2024-03-01 02:13:07 -0500

    primer commit

---
+++ TallerPOO.java
@@ -0,0 +1,57 @@
+
+class Estudiantes{
+
+//Atributos de la Clase Estudiante
+
+String Identificacion;
+String Nombre;
+String Email;
+
+//Creamos los getters y setters para obtener el valor de los atributos y difinir sus valores
+
+public void setIdentificacion(String identificacion) {
+    Identificacion = identificacion;
+}
+
+public void setNombre(String nombre) {
+    Nombre = nombre;
+}
+
+public void setEmail(String email) {
+    Email = email;
+}
+
+public String getIdentificacion() {
+    return Identificacion;
+}
+
+public String getNombre() {
+    return Nombre;
+}
+
+public String getEmail() {
+    return Email;
+}
+
+//Metodo Constructor
+
+public Estudiantes(){
+this.Email= Email;
+this.Nombre= Nombre;
+this.Identificacion= Identificacion;
+}
+
+
+
+
+
+
+
+}
+
+
+
+public class TallerPOO {
+
+    
+}
